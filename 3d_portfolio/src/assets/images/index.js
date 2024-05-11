7bad4282ad9b33a83084b86c3dbf8b4048f45b3b
Jayesh805 <jayeshkumawat8080@gmail.com>
2024-05-11 14:17:52 +0530

    added

---
+++ 3d_portfolio/src/assets/images/index.js
@@ -0,0 +1,19 @@
+import codeB from './codeB.png'
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import Alma from './Alma.png'
+
+export {
+    Alma,
+    codeB,
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo
+}
